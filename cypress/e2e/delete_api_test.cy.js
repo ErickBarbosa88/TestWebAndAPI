be62cd02c48189ea5a9c9c664a4be52d6325611d
ErickBarbosa88 <barbosaerick88@gmail.com>
2024-03-07 12:21:44 -0300

    Fix already exists email bug

--- cypress/e2e/delete_api_test.cy.js
+++ cypress/e2e/delete_api_test.cy.js
@@ -1,17 +1,17 @@
-describe('DELETE', () => {
-it('Teste para DELETE', () => {
+describe("DELETE", () => {
+  it("Teste para DELETE", () => {
     cy.request({
-        method: 'DELETE',
-        url: "https://gorest.co.in/public/v2/users/6761927", 
-        headers: {
-            Authorization: 'Bearer 6edbaee6e0843edba880055da73962236f00bb98f8f6e941b7a2c5c37c379649' 
-        },
-        failOnStatusCode: false
-    }).then(response => {
-        expect(response.status).to.eq(204);
+      method: "DELETE",
+      url: "https://gorest.co.in/public/v2/users/6761925",
+      headers: {
+        Authorization:
+          "Bearer 6edbaee6e0843edba880055da73962236f00bb98f8f6e941b7a2c5c37c379649",
+      },
+      failOnStatusCode: false,
+    }).then((response) => {
+      expect(response.status).to.eq(204);
 
-        
-        expect(response.body).to.be.empty;
+      expect(response.body).to.be.empty;
     });
+  });
 });
-})
--- cypress/e2e/post_api_test.cy.js
+++ cypress/e2e/post_api_test.cy.js
@@ -1,29 +1,28 @@
 /// <reference types="cypress"/>
 
 describe('POST', () => {
-it('Teste para POST', () => {
-    cy.request({
-        method: 'POST',
-        url: "https://gorest.co.in/public/v2/users",
-        headers: {
-            Authorization: 'Bearer 6edbaee6e0843edba880055da73962236f00bb98f8f6e941b7a2c5c37c379649'
-        },
-        body: {
-            id: 123,
-            name: "teste",
-            email: "teste@bednar.example",
-            gender: "female",
-            status: "active"
-        },
-        failOnStatusCode: false
-    }).then(response => {
-        expect(response.status).to.eq(201);
+    it('Teste para POST', () => {
+        cy.request({
+            method: 'POST',
+            url: "https://gorest.co.in/public/v2/users",
+            headers: {
+                Authorization: 'Bearer 6edbaee6e0843edba880055da73962236f00bb98f8f6e941b7a2c5c37c379649',
+            },
+            body: {
+                "name": "Novo",
+                "gender": "male",
+                "email": "dsdsdsd333567887@15ce.com",
+                "status": "active"
+            },
+            failOnStatusCode: false
+        }).then(response => {
+            console.log(response)
+            expect(response.status).to.eq(201);
 
-        expect(response.body).to.have.property('data');
-        expect(response.body.data).to.have.property('name').to.eq('teste');
-        expect(response.body.data).to.have.property('email').to.eq('teste@bednar.example');
-        expect(response.body.data).to.have.property('gender').to.eq('female');
-        expect(response.body.data).to.have.property('status').to.eq('active');
+            expect(response.body).to.have.property('name').to.eq('Novo');
+            expect(response.body).to.have.property('email').to.eq('dsdsdsd333567887@15ce.com');
+            expect(response.body).to.have.property('gender').to.eq('male');
+            expect(response.body).to.have.property('status').to.eq('active');
+        });
     });
 });
-})
